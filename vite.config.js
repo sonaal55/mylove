9e0f59181a5f25fed9f68283fbf2c03870fbacf8
Your Name <raosonaal@gmail.com>
2026-02-06 14:52:34 +0530

    Update base path for mylove repository

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VERCEL ? '/' : '/Valentine-/',
+  base: process.env.VERCEL ? '/' : '/mylove/',
   assetsInclude: ['**/*.mpeg', '**/*.mp3', '**/*.wav', '**/*.ogg'],
 })
